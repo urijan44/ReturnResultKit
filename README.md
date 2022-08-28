f87e89b8bceff963df0935009ce0d042f97e3bc9
urijan44 <teamsva360@gmail.com>
2022-08-28 17:42:42 +0900

    init proj

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
 # ReturnResultKit
 
-A description of this package.
+# Introduce
+
+Return Result Kit은 Return Type을 반환값으로 사용할 수 있는 라이브러리입니다.
+
+# Example
+- 비동기 코드에서 결과값을 ReturnResult를 통해서 꺼내올 수 있습니다.
+
+```swift
+func fetchImage(name: String) -> ReturnResult<Data, Error> {
+  return ReturnResult<Data, Error> { [unowned self] promise in
+    let url = imageService.makeURL(name: name)
+    urlSession.dataTask(with: url) { data, response, error in
+      if let error = error {
+        promise(.failure(error))
+        return
+      }
+     if let data = data {
+        promise(.success(data))
+        return
+      }
+    }
+    .resume()
+  }
+}
+```
+
+sink 인스턴스 메서드를 통해 성공값과 실패했을 때 에러값을 가져올 수 있습니다.
+```swift
+let fetchResult = configuration.fetchImage(name: "영화사진")
+fetchResult
+  .sink { [unowned imageView] result in
+  switch result {
+    case .success(let image):
+      Task {
+        imageView.image = image
+      }
+    case .failure(let error):
+      print("ERROR:" , error.localizedDescription)
+  }
+}
+```
+
+map 인스턴스 메서드를 통해 타입이 다른 ReturnResult를 생성할 수 있습니다.
+```swift
+fetchResult
+  .map { data in
+    UIImage(data: data)
+  }
+  // ReturnResult<UIImage, Error>
+  .sink { [unowned imageView] result in
+    switch result {
+      case .success(let image):
+        Task {
+          imageView.image = image
+        }
+      case .failure(let error):
+        print("ERROR:" , error.localizedDescription)
+  }
+}
+```
--- Sources/ReturnResultKit/ReturnResultKit.swift
+++ Sources/ReturnResultKit/ReturnResultKit.swift
@@ -1,6 +1,60 @@
-public struct ReturnResultKit {
-    public private(set) var text = "Hello, World!"
+//
+//  ReturnResult.swift
+//
+//  Created by hoseung Lee on 2022/08/28.
+//
+import Foundation
 
-    public init() {
+public typealias SimplePromise<Output, Failure: Error> = (Result<Output, Failure>) -> Void
+
+/// Result Type을 반환값으로 사용할 수 있는 레퍼 속성
+public struct ReturnResult<Output, Failure> where Failure: Error {
+
+  private var result: (@escaping SimplePromise<Output, Failure>) -> Void
+
+  /// SimplePromise 생성자
+  /// - Parameter promise: promise 클로져 안에서 promise를 호출하고 success 또는 failure를 작성할 수 있습니다.
+  /// ```swift
+  /// func fetchImage(name: String) -> ReturnResult<Data, Error> {
+  ///   return ReturnResult<Data, Error> { [unowned self] promise in
+  ///     let url = imageService.makeURL(name: name)
+  ///     urlSession.dataTask(with: url) { data, response, error in
+  ///     if let error = error {
+  ///       promise(.failure(error))
+  ///       return
+  ///     }
+  ///     if let data = data {
+  ///       promise(.success(data))
+  ///       return
+  ///     }
+  ///   }
+  ///   .resume()
+  ///   }
+  /// }
+  ///```
+  public init(promise: @escaping (@escaping SimplePromise<Output, Failure>) -> Void) {
+    result = promise
+  }
+
+  /// map 연산자를 이용해서 다른 ReturnResult 타입을 전달할 수 있습니다.
+  /// - Parameter transform: swift 의 map처럼 사용하실 수 있습니다.
+  /// - Returns: 변환된 ReturnResult 타입
+  public func map<NewOutput>(_ transform: @escaping (Output) -> NewOutput) -> ReturnResult<NewOutput, Failure> {
+    ReturnResult<NewOutput, Failure> { newPromise in
+      result { newResult in
+        switch newResult {
+          case .success(let newOutput):
+            newPromise(.success(transform(newOutput)))
+          case .failure(let error):
+            newPromise(.failure(error))
+        }
+      }
     }
+  }
+
+  /// 저장된 값을 sink를 통해서 가져올 수 있습니다.
+  /// - Parameter result: result는 success, failure 두 타입이 있습니다.
+  public func sink(result: @escaping SimplePromise<Output, Failure>) {
+    self.result(result)
+  }
 }
