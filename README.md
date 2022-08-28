ad581976b69bff9435091ecc5a3e33ec2c598171
urijan44 <teamsva360@gmail.com>
2022-08-28 17:23:49 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ReturnResultKit
+
+A description of this package.
---
+++ Sources/ReturnResultKit/ReturnResultKit.swift
@@ -0,0 +1,6 @@
+public struct ReturnResultKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
